e5e6bae72faaaafc53c2df3ce73840d7eb025d3f
Jacob Dohner <dohner7@gmail.com>
2017-02-20 20:32:43 -0500

    changes

--- CheckPriv/app/src/main/java/com/dohner/myapplication/MainActivity.java
+++ CheckPriv/app/src/main/java/com/dohner/myapplication/MainActivity.java
@@ -2,6 +2,9 @@ package com.dohner.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageButton button = (ImageButton) findViewById(R.id.button);
+        final TextView text = (TextView) findViewById(R.id.textView);
+        text.setVisibility(View.INVISIBLE);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                checkPriviledge(button,text);
+            }
+
+    });
+
+
+}
+
+    public static void checkPriviledge(ImageButton button, TextView text) {
+
+        button.setVisibility(View.GONE);
+        text.setVisibility(View.VISIBLE);
+
     }
+
 }
